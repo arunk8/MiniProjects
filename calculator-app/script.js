b2b6ccd7084f91ca7408fd483ead562acd80fbc1
arun <arunraj963686@gmail.com>
2023-03-06 13:36:23 +0000

    updates added

--- calculator-app/script.js
+++ calculator-app/script.js
@@ -1,3 +1,11 @@
 function display(value) {
     value!="" ? document.getElementById("result").value += value :document.getElementById("result").value="";
     }
+
+
+    function solve() {
+        let x = document.getElementById("result").value
+        let y = math.evaluate(x)
+        document.getElementById("result").value = y
+        console.log(y);
+    }
