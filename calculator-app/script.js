ea30ef23d3a4c4a93d0d580db6ad90d77d44777b
arun <arunraj963686@gmail.com>
2023-03-08 04:29:45 +0000

    calculator app

--- calculator-app/script.js
+++ calculator-app/script.js
@@ -1,11 +1,27 @@
-function display(value) {
-    value!="" ? document.getElementById("result").value += value :document.getElementById("result").value="";
-    }
+// function display(value) {
+//     value!="" ? document.getElementById("result").value += value :document.getElementById("result").value="";
+//     }
 
 
-    function solve() {
-        let x = document.getElementById("result").value
-        let y = math.evaluate(x)
-        document.getElementById("result").value = y
-        console.log(y);
-    }
+    // function solve() {
+    //     let x = document.getElementById("result").value
+    //     let y = math.evaluate(x)
+    //     document.getElementById("result").value = y
+    //     console.log(y);
+    // }
+
+   let string = "";
+    let buttons = document.querySelectorAll('.button');
+    Array.from(buttons).forEach((button) => {
+        button.addEventListener('click', (e) =>{
+            if(e.target.innerHTML == '=' ) {
+                string = eval(string);
+                document.getElementsByClassName('display').value = string;
+            }
+            else{ 
+                console.log(e.target)
+                string = string + e.target.innerHTML;
+                document.getElementsByClassName('display').value = string;
+            }
+        })
+    })
